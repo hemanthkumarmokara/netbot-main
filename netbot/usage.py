5e5f5fee2045d6eb491afaf78c1cffa72e3bc48c
Hemanth Kumar <100550244+hemanthkumarmokara@users.noreply.github.com>
2025-05-27 12:13:56 +0530

    Update usage.py

--- netbot/usage.py
+++ netbot/usage.py
@@ -1,11 +1,1 @@
-import sys
-import os
-
-# Add the absolute path to the `newtnetbot` folder
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'newtnetbot')))
-
-# Now import directly
-from appcopy import lol
-
-# Use the function
-print(lol(32, 45))
+print("hi I am hemanth")
